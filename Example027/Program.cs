898287bd01ab1e9ce2542a5d31ddd3e5d7a26dba
iam6adman <iam6adman@outlook.com>
2023-04-22 21:15:56 +0300

    add Example 027

---
+++ Example027/Program.cs
@@ -0,0 +1,35 @@
+﻿// Функция подсчета цифр 
+int NumLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр 
+void SumNum(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
+}
+
+// Функция ввода
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int num = ReadInt("Введите число ");
+
+int len = NumLen(num);
+SumNum(num, len);
