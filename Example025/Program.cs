be9419d81d6dd902747f624c079327eb8455f83e
iam6adman <iam6adman@outlook.com>
2023-04-22 21:08:54 +0300

    add Example 025

---
+++ Example025/Program.cs
@@ -0,0 +1,22 @@
+﻿// Функция возведения в степень
+void Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(a + " в степени " + b + " = " + result);
+}
+
+// Функция ввода
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int numberA = ReadInt("Введите число ");
+int numberB = ReadInt("Введите степень ");
+Degree(numberA, numberB);
+
